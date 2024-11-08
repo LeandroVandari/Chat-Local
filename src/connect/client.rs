628ef793c621ab1cb333f7ad1eca733625499ed4
LeandroVandari <bernardokreutz@gmail.com>
2024-11-07 21:01:08 -0300

    Multicast connection done

--- src/connect/client.rs
+++ src/connect/client.rs
@@ -1,1 +1,19 @@
+use std::net::UdpSocket;
+use super::addrs;
 
+pub struct Client {
+    udp_sock: UdpSocket
+}
+
+impl Client {
+    pub fn new() -> Self {
+        let udp_sock = UdpSocket::bind((std::net::Ipv4Addr::UNSPECIFIED, 0)).expect("Couldn't create UDP Socket");
+
+        udp_sock.join_multicast_v4(&addrs::MULTICAST_IPV4, &std::net::Ipv4Addr::UNSPECIFIED).expect("Couldn't join multicast");
+
+
+        udp_sock.send_to(b"Hi", &addrs::SOCKET_ADDR).unwrap();
+
+        Self { udp_sock }
+    }
+}
