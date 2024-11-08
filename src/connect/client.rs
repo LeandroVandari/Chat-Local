a88f8f1193fe5879637d409a609c49b16ee62c8d
LeandroVandari <bernardokreutz@gmail.com>
2024-11-08 20:34:04 -0300

    Fixed client problem?

--- src/connect/client.rs
+++ src/connect/client.rs
@@ -1,5 +1,7 @@
+use log::debug;
+
 use super::addrs;
-use std::net::{TcpListener, TcpStream, UdpSocket};
+use std::{net::{TcpListener, TcpStream, UdpSocket}, thread, time::Duration};
 
 pub struct Client {
     _server_conn: TcpStream,
@@ -19,17 +21,26 @@ impl Client {
             udp_sock.local_addr().unwrap().port(),
         ))
         .expect("Couldn't create listener");
-        let request_json = serde_json::to_string(&super::ConnectionRequest).unwrap();
-
-        udp_sock
-            .send_to(request_json.as_bytes(), addrs::SOCKET_ADDR) // FIXME: Since this is UDP, whenever the server is busy it might not receive the msg
-            .expect("Couldn't send connection request to server");
-
-        let (server_conn, _addr) = listener.accept().unwrap();
+        listener.set_nonblocking(true).expect("Can't make a non-blocking TcpListener");
+    
+        Self::send_conn_request(&udp_sock);
+        let mut before_accept = std::time::Instant::now();
+        let (server_conn, _addr) = {
+            let mut accept_result = listener.accept();
+            while accept_result.is_err() {thread::sleep(Duration::from_millis(10)); accept_result = listener.accept(); if before_accept.elapsed() > Duration::from_secs(2) {Self::send_conn_request(&udp_sock); before_accept=std::time::Instant::now(); debug!("2 seconds elapsed since connection request... Sending new one.")}}
+            accept_result.unwrap()
+        };
+        
 
         Self {
             _server_conn: server_conn,
         }
+    }
+
+    fn send_conn_request(udp_sock: &UdpSocket) {
+        udp_sock
+            .send_to(&super::CONN_REQUEST, addrs::SOCKET_ADDR)
+            .expect("Couldn't send connection request to server");
     }
 }
 
