44b35fb3c4f7d7831d2e05a89b78c9becd2e08ea
LeandroVandari <bernardokreutz@gmail.com>
2024-11-09 16:46:44 -0300

    added tests

--- src/connect/mod.rs
+++ src/connect/mod.rs
@@ -11,3 +11,22 @@ pub mod server;
 struct ConnectionRequest;
 
 const CONN_REQUEST: LazyCell<Vec<u8>> = LazyCell::new(|| serde_json::to_string(&ConnectionRequest).unwrap().as_bytes().to_vec());
+
+
+#[cfg(test)]
+mod tests {
+    use std::net::UdpSocket;
+
+    #[test]
+    fn can_bind_to_udp() {
+        UdpSocket::bind(super::addrs::SOCKET_ADDR).unwrap();
+    }
+
+    #[test]
+    fn can_join_multicast() {
+        let sock = UdpSocket::bind(super::addrs::SOCKET_ADDR).unwrap();
+        sock
+            .join_multicast_v4(&super::addrs::MULTICAST_IPV4, &std::net::Ipv4Addr::UNSPECIFIED)
+            .unwrap();
+    }
+}
